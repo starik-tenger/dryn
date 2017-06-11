58d00627382d0ad7bd524f3eaca7f2622fd02c8c
Andrey Ilyin <ilyinandreyilyin@yandex.ru>
2017-06-11 22:10:55 +0300

    posts: timeout

--- posts/script.js
+++ posts/script.js
@@ -1,3 +1,11 @@
+function getCookie(name) {
+  var matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+
 var adress = "http://192.168.0.101:8000";
 
 var XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
@@ -5,10 +13,12 @@ var XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
 var xhr = new XHR();
 xhr.withCredentials = false;
 
+xhr.timeout = 30000;
+
 var input = document.getElementById("p");
 var p = document.getElementById("p1");
 
-
+var id = 0;
 
 xhr.onload = function() {
   //alert( this.responseText );
@@ -19,10 +29,19 @@ xhr.onerror = function() {
   alert( 'Ошибка ' + this.status );
 }
 
+
 function f()
 {
-	adress = adress + "?" + input.value;
-	xhr.open('GET', adress, true);
+	var adress1 = adress + "?" + input.value;
+	xhr.open('GET', adress1, true);
 	xhr.send();
 	adress = "http://192.168.0.101:8000";
 }
+
+function f1()
+{
+	xhr.open('GET', adress + "?test", true);
+	xhr.send();
+}
+
+setInterval(f1, 1000);
